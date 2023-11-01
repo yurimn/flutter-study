d9637279d2806429fa870cad4503f7182bf3d55a
Cha-junyoung <ckwnsdud001@gmail.com>
2023-11-01 22:43:31 +0900

    23.11.01.22:43

--- to_do_list/lib/main.dart
+++ to_do_list/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'theme/junyoung/junyoung.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,12 +10,10 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
-    );
+    return MaterialApp(
+        home: const BasicApp(),
+        theme: ThemeData(
+          primarySwatch: Colors.grey,
+        ));
   }
 }
