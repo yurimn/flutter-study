12a968fdcfaf0edf4daa43c589566c337975ff1f
HyungSeok1 <129472415+HyungSeok1@users.noreply.github.com>
2023-11-07 13:00:58 +0900

    week2 homework(change style)

---
+++ to_do_list/lib/theme/hyungseok/month_8.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MenuMonth8 extends StatefulWidget {
+  const MenuMonth8({Key? key}) : super(key: key);
+
+  @override
+  MenuMonthState createState() => MenuMonthState();
+}
+
+class MenuMonthState extends State<MenuMonth8> {
+  @override
+  Widget build(BuildContext context) {
+    return const Text("Month");
+  }
+}
