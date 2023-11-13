83bff06b46631e2bb9111eb156e1a579d173a23f
Dahye Won <doall1102@gmail.com>
2023-11-13 23:36:15 +0900

    시작, 종료 시간 추가 및 image picker 기능

--- to_do_list/lib/theme/dahye/list_4.dart
+++ to_do_list/lib/theme/dahye/list_4.dart
@@ -10,46 +10,11 @@ class MenuList extends StatefulWidget {
 
 class MenuListState extends State<MenuList> {
   List listComponent = [
-    menuComponent("Flutter HW", "휴지통 기능 추가"),
-    menuComponent("Math HW", "전산수학 Ch.7"),
-    menuComponent("Bass", "<우리의 꿈> 연습"),
-    menuComponent("Study OS", "운영체제 Ch.6"),
+    ["title1", "content1", "2023-10-10 12:27:00", "2023-10-11 13:27:00"],
+    ["title2", "content2", "2023-10-10 12:27:00", "2023-10-11 13:27:00"],
+    ["title3", "content3", "2023-10-10 12:27:00", "2023-10-11 13:27:00"],
+    ["title4", "content4", "2023-10-10 12:27:00", "2023-10-11 13:27:00"],
   ];
-
-  Widget deleteButton(int index) {
-    return TextButton(
-      onPressed: () {
-        showDialog(
-          context: context,
-          builder: (BuildContext context) {
-            return AlertDialog(
-              title: const Text("할 일 삭제"),
-              content: const Text("삭제하시겠습니까?"),
-              actions: <Widget>[
-                TextButton(
-                  onPressed: () {
-                    Navigator.of(context).pop();
-                  },
-                  child: const Text("취소"),
-                ),
-                TextButton(
-                  onPressed: () {
-                    setState(() {
-                      // 해당 인덱스의 할 일 삭제
-                      listComponent.removeAt(index);
-                      Navigator.of(context).pop(); // 다이얼로그 닫기
-                    });
-                  },
-                  child: const Text("삭제"),
-                ),
-              ],
-            );
-          },
-        );
-      },
-      child: const Icon(Icons.delete, color: Color.fromRGBO(0, 32, 74, 50)),
-    );
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -63,71 +28,97 @@ class MenuListState extends State<MenuList> {
                 showDialog(
                     context: context,
                     builder: (BuildContext context) {
-                      return addToDoDialog(context,
-                          (String title, String content) {
+                      return addToDoDialog(context, (String title,
+                          String content, String startTime, String endTime) {
                         setState(() {
-                          listComponent.add(menuComponent(title, content));
+                          listComponent
+                              .add([title, content, startTime, endTime]);
                         });
-                      });
+                      }, setState);
                     });
               });
             },
             style: ElevatedButton.styleFrom(
-              backgroundColor: Color.fromARGB(250, 254, 190, 73),
+              backgroundColor: const Color.fromARGB(250, 254, 190, 73),
               shape: const RoundedRectangleBorder(
                   borderRadius: BorderRadius.all(Radius.circular(10))),
             ),
-            child: Icon(Icons.add, color: Colors.white),
+            child: const Icon(Icons.add, color: Colors.white),
           ));
     }
 
-    List<Widget> menuComponentsWithDeleteButtons = [];
-
-    for (int i = 0; i < listComponent.length; i++) {
-      menuComponentsWithDeleteButtons.add(Padding(
-        padding: const EdgeInsets.only(top: 5),
-        child: Container(
-          decoration: BoxDecoration(
-            color: Colors.white,
-            border: Border.all(
-                color: Color.fromARGB(250, 254, 190, 73), width: 1.5),
-            borderRadius: const BorderRadius.all(Radius.circular(10)),
-          ),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              listComponent[i],
-              deleteButton(i),
-            ],
-          ),
-        ),
-      ));
+    Widget deleteButton(int index, Function() onPressed) {
+      return IconButton(
+        onPressed: onPressed,
+        style: ButtonStyle(
+            backgroundColor: MaterialStateProperty.all(Colors.white),
+            shape: MaterialStateProperty.all(const CircleBorder()),
+            iconSize: MaterialStateProperty.all(25),
+            alignment: Alignment.center),
+        padding: const EdgeInsets.all(0),
+        icon: const Icon(Icons.delete, color: Colors.red),
+      );
     }
 
     return Padding(
-      padding: const EdgeInsets.all(20.0),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          addButton(),
-          // 할 일 목록과 삭제 버튼을 추가
-          ...menuComponentsWithDeleteButtons,
-        ],
-      ),
-    );
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            addButton(),
+            ListView.builder(
+                shrinkWrap: true,
+                itemCount: listComponent.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return menuComponent(
+                      context,
+                      listComponent[index],
+                      deleteButton(index, () {
+                        setState(() {
+                          listComponent.removeAt(index);
+                        });
+                      }));
+                })
+          ],
+        ));
   }
 }
 
-Widget menuComponent(String title, String content) {
-  return Container(
-      height: 50,
-      padding: const EdgeInsets.all(10),
-      child: Row(
-        children: [
-          Text(title, style: const TextStyle(fontWeight: FontWeight.bold)),
-          const SizedBox(width: 10),
-          Text(content),
-        ],
-      ));
+Widget menuComponent(BuildContext context, List content, Widget deleteButton) {
+  return Padding(
+      padding: const EdgeInsets.only(top: 5),
+      child: ElevatedButton(
+          onPressed: () {
+            showDialog(
+                context: context,
+                builder: (BuildContext context) {
+                  return viewToDoDialog(context, content);
+                });
+          },
+          style: ElevatedButton.styleFrom(
+            padding: const EdgeInsets.all(0),
+            shape: const RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(Radius.circular(10))),
+          ),
+          child: Container(
+              height: 50,
+              padding: const EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                border: Border.all(
+                    color: const Color.fromARGB(250, 254, 190, 73), width: 1.5),
+                borderRadius: const BorderRadius.all(Radius.circular(10)),
+              ),
+              child: Row(
+                children: [
+                  Text(content[0],
+                      style: const TextStyle(
+                          color: Colors.black, fontWeight: FontWeight.bold)),
+                  const SizedBox(width: 10),
+                  Text(content[1], style: const TextStyle(color: Colors.grey)),
+                  const Spacer(),
+                  deleteButton
+                ],
+              ))));
 }
--- to_do_list/lib/theme/dahye/util.dart
+++ to_do_list/lib/theme/dahye/util.dart
@@ -1,25 +1,180 @@
 import "package:flutter/material.dart";
 
-Widget addToDoDialog(BuildContext context, Function add) {
-  TextEditingController titleController = TextEditingController();
-  TextEditingController contentController = TextEditingController();
+Widget addToDoDialog(BuildContext context, Function add, Function setState) {
+  // title, content, startTime, endTime to be added by textformfield
+
+  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
+  final TextEditingController titleController = TextEditingController();
+  final TextEditingController contentController = TextEditingController();
+  TextEditingController startDateController = TextEditingController();
+  TextEditingController startTimeController = TextEditingController();
+  TextEditingController endDateController = TextEditingController();
+  TextEditingController endTimeController = TextEditingController();
+
   return AlertDialog(
-    title: const Text("할 일 추가"),
+      title: const Text("Add ToDo"),
+      content: Form(
+        key: formKey,
+        child: SizedBox(
+            height: MediaQuery.of(context).size.height * 0.4,
+            child: Column(
+              children: [
+                TextFormField(
+                  controller: titleController,
+                  decoration: const InputDecoration(
+                    labelText: "제목",
+                    hintText: "제목을 입력하세요",
+                  ),
+                  validator: (String? value) {
+                    if (value == null || value.isEmpty) {
+                      return "제목을 입력하세요";
+                    }
+                    return null;
+                  },
+                ),
+                TextFormField(
+                  controller: contentController,
+                  decoration: const InputDecoration(
+                    labelText: "내용",
+                    hintText: "내용을 입력하세요",
+                  ),
+                  validator: (String? value) {
+                    if (value == null || value.isEmpty) {
+                      return "내용을 입력하세요";
+                    }
+                    return null;
+                  },
+                ),
+                Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+                  SizedBox(
+                      height: 60,
+                      child: Padding(
+                        padding: const EdgeInsets.only(top: 15, right: 10),
+                        child: OutlinedButton(
+                          onPressed: () {
+                            showTimePicker(
+                                    context: context,
+                                    initialTime: TimeOfDay.now())
+                                .then((value) {
+                              if (value != null) {
+                                startTimeController.text =
+                                    value.toString().substring(10, 15);
+                              }
+                            });
+                            showDatePicker(
+                                    context: context,
+                                    initialDate: DateTime.now(),
+                                    firstDate: DateTime(2021),
+                                    lastDate: DateTime(2025))
+                                .then((value) {
+                              if (value != null) {
+                                startDateController.text =
+                                    value.toString().substring(0, 10);
+                              }
+                            });
+                          },
+                          child: const Text("시작"),
+                        ),
+                      )),
+                  SizedBox(
+                      height: 60,
+                      child: Padding(
+                        padding: const EdgeInsets.only(top: 15, left: 10),
+                        child: OutlinedButton(
+                          onPressed: () {
+                            showTimePicker(
+                                    context: context,
+                                    initialTime: TimeOfDay.now())
+                                .then((value) {
+                              if (value != null) {
+                                endTimeController.text =
+                                    value.toString().substring(10, 15);
+                              }
+                            });
+                            showDatePicker(
+                                    context: context,
+                                    initialDate: DateTime.now(),
+                                    firstDate: DateTime(2021),
+                                    lastDate: DateTime(2025))
+                                .then((value) {
+                              if (value != null) {
+                                endDateController.text =
+                                    value.toString().substring(0, 10);
+                              }
+                            });
+                          },
+                          child: const Text("종료"),
+                        ),
+                      )),
+                ]),
+                const Spacer(),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    minimumSize: const Size(double.infinity, 50),
+                    shape: const RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(10))),
+                  ),
+                  onPressed: () {
+                    if (formKey.currentState!.validate()) {
+                      int startdate = int.parse(startDateController.text
+                          .replaceAll("-", "")
+                          .replaceAll(" ", ""));
+                      int enddate = int.parse(endDateController.text
+                          .replaceAll("-", "")
+                          .replaceAll(" ", ""));
+                      int starttime = int.parse(
+                          startTimeController.text.replaceAll(":", ""));
+                      int endtime =
+                          int.parse(endTimeController.text.replaceAll(":", ""));
+                      if (enddate < startdate ||
+                          ((enddate == startdate) && (endtime < starttime))) {
+                        showDialog(
+                            context: context,
+                            builder: (BuildContext context) {
+                              return AlertDialog(
+                                title: const Text("오류"),
+                                content:
+                                    const Text("종료 날짜와 시간이 시작 날짜와 시간보다 빠릅니다."),
+                                actions: [
+                                  TextButton(
+                                    onPressed: () {
+                                      Navigator.pop(context);
+                                    },
+                                    child: const Text("닫기"),
+                                  )
+                                ],
+                              );
+                            });
+                      } else {
+                        add(
+                          titleController.text,
+                          contentController.text,
+                          '${startDateController.text} ${startTimeController.text}',
+                          '${endDateController.text} ${endTimeController.text}',
+                        );
+                        Navigator.pop(context);
+                      }
+                    }
+                  },
+                  child: const Text("추가"),
+                )
+              ],
+            )),
+      ));
+}
+
+Widget viewToDoDialog(BuildContext context, List content) {
+  return AlertDialog(
+    title: Text(content[0]),
     content: Column(
       mainAxisSize: MainAxisSize.min,
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        TextField(
-          controller: titleController,
-          decoration: const InputDecoration(
-            labelText: "제목",
-          ),
-        ),
-        TextField(
-          controller: contentController,
-          decoration: const InputDecoration(
-            labelText: "내용",
-          ),
-        ),
+        Text("내용  " + content[1]),
+        const SizedBox(height: 10),
+        Text("시작  " + content[2]),
+        const SizedBox(height: 10),
+        Text("종료  " + content[3]),
       ],
     ),
     actions: [
@@ -27,15 +182,8 @@ Widget addToDoDialog(BuildContext context, Function add) {
         onPressed: () {
           Navigator.pop(context);
         },
-        child: const Text("취소"),
-      ),
-      TextButton(
-        onPressed: () {
-          add(titleController.text, contentController.text);
-          Navigator.pop(context);
-        },
-        child: const Text("추가"),
-      ),
+        child: const Text("닫기"),
+      )
     ],
   );
 }
