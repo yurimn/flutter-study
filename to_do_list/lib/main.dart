87c8a59e11dc3e3cdc5cf6ed7ece72c4d15f607c
yurimn <leeyou6757@gmail.com>
2023-11-20 00:05:39 +0900

    add icon

--- to_do_list/lib/main.dart
+++ to_do_list/lib/main.dart
@@ -1,8 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 //import 'main_junyoung.dart';
 //import 'main_dahye.dart';
-import 'main_basic.dart';
+// import 'main_basic.dart';
 
 void main() {
   runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final Future<SharedPreferences> _prefs = SharedPreferences.getInstance();
+    Future<void> _loadData() async {
+      final SharedPreferences prefs = await _prefs;
+    }
+
+    //return const MainApp();
+    return const MainApp();
+  }
 }
